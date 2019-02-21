46a8afda5d3b1c9dad296967ec52ba1baedf7d10
akhodas <a.l.khodas@gmail.com>
2019-02-21 04:04:44 +0300

    refactoring createCard where add function for feild days color hashtag

--- src/main.js
+++ src/main.js
@@ -164,129 +164,14 @@ function drawCards(configCards) {
 
                       <fieldset class="card__repeat-days">
                         <div class="card__repeat-days-inner">
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-mo-6"
-                            name="repeat"
-                            value="mo"
-                          />
-                          <label class="card__repeat-day" for="repeat-mo-6"
-                            >mo</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-tu-6"
-                            name="repeat"
-                            value="tu"
-                            checked
-                          />
-                          <label class="card__repeat-day" for="repeat-tu-6"
-                            >tu</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-we-6"
-                            name="repeat"
-                            value="we"
-                          />
-                          <label class="card__repeat-day" for="repeat-we-6"
-                            >we</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-th-6"
-                            name="repeat"
-                            value="th"
-                          />
-                          <label class="card__repeat-day" for="repeat-th-6"
-                            >th</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-fr-6"
-                            name="repeat"
-                            value="fr"
-                            checked
-                          />
-                          <label class="card__repeat-day" for="repeat-fr-6"
-                            >fr</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            name="repeat"
-                            value="sa"
-                            id="repeat-sa-6"
-                          />
-                          <label class="card__repeat-day" for="repeat-sa-6"
-                            >sa</label
-                          >
-                          <input
-                            class="visually-hidden card__repeat-day-input"
-                            type="checkbox"
-                            id="repeat-su-6"
-                            name="repeat"
-                            value="su"
-                            checked
-                          />
-                          <label class="card__repeat-day" for="repeat-su-6"
-                            >su</label
-                          >
+                          ${createFeildRepeatDays()}                          
                         </div>
                       </fieldset>
                     </div>
 
                     <div class="card__hashtag">
                       <div class="card__hashtag-list">
-                        <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <button type="button" class="card__hashtag-name">
-                            #repeat
-                          </button>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>
-
-                        <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <button type="button" class="card__hashtag-name">
-                            #cinema
-                          </button>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>
-
-                        <span class="card__hashtag-inner">
-                          <input
-                            type="hidden"
-                            name="hashtag"
-                            value="repeat"
-                            class="card__hashtag-hidden-input"
-                          />
-                          <button type="button" class="card__hashtag-name">
-                            #entertaiment
-                          </button>
-                          <button type="button" class="card__hashtag-delete">
-                            delete
-                          </button>
-                        </span>
+                        ${createFeildCardHashtagList()}
                       </div>
 
                       <label>
@@ -316,71 +201,7 @@ function drawCards(configCards) {
                   <div class="card__colors-inner">
                     <h3 class="card__colors-title">Color</h3>
                     <div class="card__colors-wrap">
-                      <input
-                        type="radio"
-                        id="color-black-6"
-                        class="card__color-input card__color-input--black visually-hidden"
-                        name="color"
-                        value="black"
-                        ${(configCard.color === `black`) ? `checked` : ``}
-                      />
-                      <label
-                        for="color-black-6"
-                        class="card__color card__color--black"
-                        >black</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-yellow-6"
-                        class="card__color-input card__color-input--yellow visually-hidden"
-                        name="color"
-                        value="yellow"
-                        ${(configCard.color === `yellow`) ? `checked` : ``}
-                      />
-                      <label
-                        for="color-yellow-6"
-                        class="card__color card__color--yellow"
-                        >yellow</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-blue-6"
-                        class="card__color-input card__color-input--blue visually-hidden"
-                        name="color"
-                        value="blue"
-                        ${(configCard.color === `blue`) ? `checked` : ``}
-                      />
-                      <label
-                        for="color-blue-6"
-                        class="card__color card__color--blue"
-                        >blue</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-green-6"
-                        class="card__color-input card__color-input--green visually-hidden"
-                        name="color"
-                        value="green"
-                        ${(configCard.color === `green`) ? `checked` : ``}
-                      />
-                      <label
-                        for="color-green-6"
-                        class="card__color card__color--green"
-                        >green</label
-                      >
-                      <input
-                        type="radio"
-                        id="color-pink-6"
-                        class="card__color-input card__color-input--pink visually-hidden"
-                        name="color"
-                        value="pink"
-                        ${(configCard.color === `pink`) ? `checked` : ``}
-                      />
-                      <label
-                        for="color-pink-6"
-                        class="card__color card__color--pink"
-                        >pink</label
-                      >
+                      ${createFeildCardColorWrap(configCard.color)}
                     </div>
                   </div>
                 </div>
@@ -395,6 +216,89 @@ function drawCards(configCards) {
     `;
 
     return result;
+  }
+
+  function createFeildRepeatDays() {
+    function createRepeatDays() {
+      const days = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+      return days.map(createRepeatDay).join(``);
+    }
+
+    function createRepeatDay(day) {
+      const result = `
+        <input
+        class="visually-hidden card__repeat-day-input"
+        type="checkbox"
+        id="repeat-${day}-6"
+        name="repeat"
+        value="${day}"
+        ${(Math.random() - 0.5) > 0 ? `checked` : ``}
+        />
+        <label class="card__repeat-day" for="repeat-${day}-6"
+          >${day}</label
+        >
+      `;
+      return result;
+    }
+
+    return createRepeatDays();
+  }
+
+  function createFeildCardColorWrap(configColor) {
+    function createCardsColorWrap() {
+      const cardColor = [`black`, `yellow`, `green`, `blue`, `pink`];
+      return cardColor.map(createCardColorWrap).join(``);
+    }
+
+    function createCardColorWrap(color) {
+      const result = `
+        <input
+          type="radio"
+          id="color-${color}-6"
+          class="card__color-input card__color-input--${color} visually-hidden"
+          name="color"
+          value="${color}"
+          ${(configColor === color) ? `checked` : ``}
+        />
+        <label
+          for="color-${color}-6"
+          class="card__color card__color--${color}"
+          >${color}</label
+        >
+      `;
+      return result;
+    }
+
+    return createCardsColorWrap();
+  }
+
+  function createFeildCardHashtagList() {
+    function createCardHashtagList() {
+      const hashtagList = [`#repeat`, `#cinema`, `#entertaiment`];
+      return hashtagList.map(createCardHashtag).join(``);
+    }
+
+    function createCardHashtag(hashtag) {
+      const result = `
+        <span class="card__hashtag-inner">
+          <input
+            type="hidden"
+            name="hashtag"
+            value="repeat"
+            class="card__hashtag-hidden-input"
+          />
+          <button type="button" class="card__hashtag-name">
+            ${hashtag}
+          </button>
+          <button type="button" class="card__hashtag-delete">
+            delete
+          </button>
+        </span>
+      `;
+      return result;
+    }
+
+    return createCardHashtagList();
   }
 }
 
