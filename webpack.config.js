5834857410ffd247cd19e04c65e4655c4f42a8a6
akhodas <a.l.khodas@gmail.com>
2019-02-23 18:22:36 +0300

    add webpack and eslint rules also start npm run build

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    // eslint-disable-next-line no-undef
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`
+};
